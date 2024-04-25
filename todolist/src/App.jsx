cbebf6c773e9cd5f82d7bf110d62442ef11ea1dd
sin_yu <catmoon1017@gmail.com>
2024-04-26 02:31:08 +0800

    feat: 🎸 todolist、props練習

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,48 +1,63 @@
 import { useState } from 'react';
+import { ListItem } from './component/Item';
+
 export function App() {
-	const [todoList, setTodoList] = useState(['hello', 'test']);
-	const [state, setState] = useState(0);
-
-	function handleCount(e) {
-		//# 此處收到的e 會是event 事件資料
-		setState(e.target.value);
+	const [todoList, setTodoList] = useState([
+		{ text: '1fffffff23', checked: false },
+	]);
+	const [inputText, setInputText] = useState('');
+	function handleChecked(index) {
+		const changeCheckedList = [...todoList];
+		changeCheckedList[index].checked = !changeCheckedList[index].checked;
+		setTodoList(changeCheckedList);
 	}
-	function handelSubmit() {
-		// const newList = [...todoList, state];
-
-		// setTodoList(newList);
-		setTodoList((currentTodo) => {
-			return [...currentTodo, state];
-		});
+	function handleChangeInputText(e) {
+		setInputText(e.target.value);
+	}
+	function handleUpdateTodoList() {
+		const newTodoList = [...todoList, { text: inputText, checked: false }];
+		setTodoList(newTodoList);
+		setInputText('');
 	}
 
 	return (
-		<>
+		<div
+			style={{
+				width: '100vw',
+				height: '100vw',
+				backgroundColor: '#FFFF',
+				color: '#000',
+			}}>
 			<div
 				style={{
 					margin: '4rem',
-					display: 'flex',
-					alignItem: 'center',
-					flexDirection: 'column',
 				}}>
-				<input
-					type="text"
-					onChange={handleCount}
-					style={{ marginBottom: '10px' }}
-				/>
-				<button onClick={handelSubmit}>submit</button>
-				{todoList.map((item, index) => (
-					<div
-						style={{ display: 'flex' }}
-						key={item}>
-						<input type="checkbox" />
-						<div>
-							#{index + 1}-{item}
-						</div>
-					</div>
-				))}
+				<div>
+					<input
+						style={{ marginBottom: '20px', marginRight: '20px' }}
+						type="text"
+						value={inputText}
+						onChange={handleChangeInputText}
+					/>
+					<button onClick={handleUpdateTodoList}>新增</button>
+				</div>
+				<ul
+					style={{
+						display: 'flex',
+						alignItem: 'center',
+						flexDirection: 'column',
+					}}>
+					{todoList.map((item, index) => (
+						<ListItem
+							content={item.text}
+							checked={item.checked}
+							key={item.text}
+							onChecked={() => handleChecked(index)}
+						/>
+					))}
+				</ul>
 			</div>
-		</>
+		</div>
 	);
 }
 
